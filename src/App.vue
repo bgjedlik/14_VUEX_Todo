378ce6be38119702d257130b7bcb89d36c05ae9e
Bólya Gábor <bolya.gabor@gmail.com>
2022-06-25 12:31:16 +0200

    v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="container">ok</div>
+  <div class="container">
+    <app-header />
+    <app-main />
+  </div>
 </template>
 
 <script>
-export default {};
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+export default {
+  name: "app",
+  components: { AppHeader, AppMain },
+};
 </script>
 
 <style>
---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    state:{
+        todos:[]
+    },
+    getters:{},
+    mutations:{
+        saveTodo(state, newTodo){
+            let maxId = Math.max(...state.todos.map(x => x.id))
+            state.todos.push({
+                'id': isFinite(maxId) ? maxId+1 : 1 ,
+                'text':newTodo,
+                'completed':false,
+                'isEdited':false
+            })
+        },
+        editTodo(state,todo){
+            state.todos.forEach(x => x.isEdited = false)
+            state.todos.find(x => x == todo).isEdited=true
+        },
+        doneEdit(state,todo){
+            state.todos.find(x => x == todo).isEdited=false
+        },
+        removeTodo(state,todo){
+            state.todos.splice(state.todos.indexOf(todo),1)
+        }
+
+    }
+})
